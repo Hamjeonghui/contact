f2008be4b828ff853b62f7917fb29dbc8ff6d4c7
DESKTOP-0M2RJ97\wizai <gkawjdgml15@wizai.co.kr>
2023-03-14 08:50:59 +0900

    Divide by three section(top, middle, bottom)

--- lib/main.dart
+++ lib/main.dart
@@ -9,13 +9,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
 
+    // Material이라는 위젯을 활용함으로써 Google에서 제공하는 Custom||Material Thema를 사용할 수 있게 된다.
     return MaterialApp(
-      // Center= 자식요소의 위치를 정해주는 위젯
-      // 이로써 위젯 안에 위젯을 선언할 수 있음을 증명할 수 있다.
-      home: Center(
-        // SizeBox()와 같은 용도로 사용된다.
-        // 사이즈 단위는 LP이며, 부모요소를 기준으로 하여 측정을 시작하고, 종료한다.
-        child: Container( width: 50,height: 50, color: Colors.red )
+      home: Scaffold( //상 중 하로 나누어주는 위젯
+        appBar: AppBar(), // 상단 위젯
+        body: Container(), //중앙 위젯
+        bottomNavigationBar: BottomAppBar( child:Text('가나다라') ) //하단 위젯
       )
     );
   }

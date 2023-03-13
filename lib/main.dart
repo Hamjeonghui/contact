d666460a7144486392769b37809b574e9ef60684
DESKTOP-0M2RJ97\wizai <gkawjdgml15@wizai.co.kr>
2023-03-13 15:49:26 +0900

    Make of image widget & setting

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MyApp()); //앱 구동시켜주세용
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
-
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
 
+    return MaterialApp(
+      home: Image.asset('../assets/dog.jpg') //dog.jpg로도 같은 동작 실행 가능
     );
   }
 }

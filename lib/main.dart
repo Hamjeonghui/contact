9c3bbb01f727dfc7b0ccc4c9f32fda64e17f8f6b
DESKTOP-0M2RJ97\wizai <gkawjdgml15@wizai.co.kr>
2023-03-13 16:54:06 +0900

    Make of Child widget

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,13 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
 
     return MaterialApp(
-      home: Image.asset('../assets/dog.jpg') //dog.jpg로도 같은 동작 실행 가능
+      // Center= 자식요소의 위치를 정해주는 위젯
+      // 이로써 위젯 안에 위젯을 선언할 수 있음을 증명할 수 있다.
+      home: Center(
+        // SizeBox()와 같은 용도로 사용된다.
+        // 사이즈 단위는 LP이며, 부모요소를 기준으로 하여 측정을 시작하고, 종료한다.
+        child: Container( width: 50,height: 50, color: Colors.red )
+      )
     );
   }
 }

d9810dc225659237a5bd8d272744ec91338c59cf
DESKTOP-0M2RJ97\wizai <gkawjdgml15@wizai.co.kr>
2023-03-14 09:15:23 +0900

    첫 번째 숙제 해결

--- lib/main.dart
+++ lib/main.dart
@@ -11,15 +11,26 @@ class MyApp extends StatelessWidget {
 
     return MaterialApp(
       home: Scaffold(
-        body: Row( //한 라인에 다수의 위젯을 배치할 수 있는 위젯/세로는 Column
-          mainAxisAlignment: MainAxisAlignment.center, //요소 메인방향(Row는 가로, Column은 세로) 가운데정렬. 다양한 객체로 정렬 변경가능
-          crossAxisAlignment: CrossAxisAlignment.center, //요소 반방향 가운데 정렬, 이 같은 경우는 세로 여백이 지정되어있어야함
-          children: const [//child가 아닌 children
-            Icon(Icons.star),
-            Icon(Icons.star),
-            Icon(Icons.star)
-          ]
-        ),
+
+        appBar: AppBar(title: Text('앱임')),
+
+        body: Container(child: Text('안녕')),
+
+        bottomNavigationBar: BottomAppBar(
+          child: Container(
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: const [
+                Icon(Icons.phone),
+                Icon(Icons.message),
+                Icon(Icons.contact_page)
+              ],
+            ),
+            height: 50,
+          ),
+        )
+
       )
     );
   }

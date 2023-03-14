03f345f2d97d11fc7957599bb7870881fb8070de
DESKTOP-0M2RJ97\wizai <gkawjdgml15@wizai.co.kr>
2023-03-14 10:33:37 +0900

    버튼과 버튼스타일

--- lib/main.dart
+++ lib/main.dart
@@ -14,18 +14,14 @@ class MyApp extends StatelessWidget {
 
         appBar: AppBar(title: Text('앱임')),
 
-        body: Column(
-          children: [
-            SizedBox(// container랑 차이: https://memostack.tistory.com/188
-              child: Text('안녕하소',
-                // style 하위 객체들을 통해서 text와 관련된 다양한 스타일을 부여할 수 있다.
-                //style: TextStyle( color: Color(0xfff52929) ),//text에 스타일 주는 법&헥터코드로 색상 주는 법(길본값0xff)
-                style: TextStyle( color: Color.fromRGBO(24, 0, 255, 0.2) ),//RGB&불투명도까지 주는 법
-              ),
-            ),
-            Icon(Icons.ac_unit, )//Icon의 경우 Style객체가 없어서, 간단한 설정만이 가능하다.
-          ],
-        )
+        body: SizedBox(
+          //버튼 위젯은 TextButton외에도 IconButton(필수요소 구성이 약간 다르다), ElevatedButton이 있다.
+          child: TextButton(
+              child: Text('텍스트 버튼에 들어갈 글자') ,
+              onPressed: (){}, //위의 child와 함께 필수 요소
+              style: ButtonStyle( ), //부여할 스타일
+          ),
+        ),
       )
     );
   }

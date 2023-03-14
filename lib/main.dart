6f3dfcff09db5ce49867082e1337e5597ee6423f
DESKTOP-0M2RJ97\wizai <gkawjdgml15@wizai.co.kr>
2023-03-14 10:16:34 +0900

    컨테이너 박스 정렬 2_정렬 구체화

--- lib/main.dart
+++ lib/main.dart
@@ -14,9 +14,11 @@ class MyApp extends StatelessWidget {
 
         appBar: AppBar(title: Text('앱임')),
 
-        body: Center(
+        body: Align(//Center등 외에 조금 더 구체적 정렬을 위한 위젯
+          alignment: Alignment.centerRight, //Alignment객체의 정렬과 관련된 클래스를 통해 다양한 정렬이 가능해짐
           child: Container( //우클릭+전구||alt+enter로 해당 컨테이너 정렬 바로 변경 가능
-              width: 50, height: 50, color: Colors.blue,
+            height: double.infinity,//부모(현시점에는 Scaffold)를 초과하지 않는 선에서 무한
+            width: 50, color: Colors.blue,
           ),
         ),
       )

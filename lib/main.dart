fc7155a964a088c50f9f42999e74e7c54d22104b
DESKTOP-0M2RJ97\wizai <gkawjdgml15@wizai.co.kr>
2023-03-16 10:11:23 +0900

    당근마켓 UI Clone Coding_02

--- lib/main.dart
+++ lib/main.dart
@@ -11,30 +11,39 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
         home: Scaffold(
-          appBar: AppBar(),
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            title: Text("금호동3가", style: TextStyle(color: Colors.black),),
+            actions: [
+              IconButton(onPressed: () {}, icon: Icon(Icons.search, color: Colors.black,)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.menu, color: Colors.black)),
+              IconButton(onPressed: () {}, icon: Icon(Icons.add_alert_rounded, color: Colors.black)),
+            ],
+          ),
           body: Column(
               children: [
-                SizedBox(
-                  width: double.infinity,
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                Container(
+                  height: 150,
+                  padding: EdgeInsets.all(10),
+                  child: Row(
                       children: [
-                        Image.asset("camera.png", width: 100, height: 100,),
+                        Image.asset("camera.png", width: 80, height: 80),
                         Container(
+                          margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                          width:  350,
                           child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
                             children: [
-                              TextButton(
-                                  onPressed: () {},
-                                  child: Text(
-                                    "캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)",
-                                    style: TextStyle(color: Colors.black, fontSize: 20),
-                                  )),
-                              Text("성동구 행당동·끌올 10분 전", style: TextStyle(color: Colors.grey),),
-                              Text("210,000원", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),)
+                              Text( "캐논 DSLR 100D (단렌즈, 충전기 16기가SD 포함)", style: TextStyle(color: Colors.black, fontSize: 20), ),
+                              Text("성동구 행당동·끌올 10분 전", style: TextStyle(color: Colors.grey,)),
+                              Text("210,000원", style: TextStyle(color: Colors.black, fontSize: 20, fontWeight: FontWeight.bold),),
+                              // AxisAlign이 잘 안될때는 요소에 부여된 폭을 생각해보자.
+                              Row(mainAxisAlignment: MainAxisAlignment.end,children: [
+                                Text("♡4", style: TextStyle(color: Colors.grey))
+                                ])
                             ],
                           ),
                         ),
-                        Text("♡4")
                       ],
                     )
                 ),
